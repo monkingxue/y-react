3df6cff225d374a4a68bf0d7e7138ea7d4b2909b
monkingxue <1360662499@qq.com>
2017-09-13 13:59:11 +0800

    [chore] 增加项目依赖和忽略

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+// Rollup plugins
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'build/bundle.js',
+    format: 'umd',
+    name: 'yreact'
+  },
+  sourcemap: 'inline',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    })
+  ],
+}
