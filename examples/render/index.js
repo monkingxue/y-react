aed30fe76814f21cfe5c42e9b8ae6c07f02a0025
monkingxue <1360662499@qq.com>
2017-09-13 13:59:35 +0800

    [feat] 增加测试

---
+++ examples/render/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xueyingchen.
+ */
+import { h, render } from '../../src'
+
+render((
+  <div>
+    <span class="title">第一次</span>
+    <button class="btn">测试</button>
+  </div>
+), document.body)
