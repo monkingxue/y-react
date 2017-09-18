2b54efdc4ac48b457311478c7c2e6a6f36fdcdf9
monkingxue <1360662499@qq.com>
2017-09-19 02:43:12 +0800

    [refactor] 修改测试用例

--- examples/render/index.js
+++ examples/render/index.js
@@ -1,11 +1,12 @@
 /**
  * Created by xueyingchen.
  */
-import { h, render } from '../../src'
+import { h, render } from '../../src/index'
 
 render((
   <div>
     <span class="title">第一次</span>
-    <button class="btn">测试</button>
+    <button class="btn" onClick={() => alert('WTF')}>测试</button>
+    <div>就是想试一试</div>
   </div>
 ), document.body)
